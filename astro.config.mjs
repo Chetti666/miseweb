27f1d640c28d2fc70d61b1574f09b7ba5f6d54f8
Chetti666 <a.guiachetti@gmail.com>
2026-06-30 16:45:46 -0400

    dominio.cl

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.carlogtz.com',
+  site: 'https://www.carlogtz.cl',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
